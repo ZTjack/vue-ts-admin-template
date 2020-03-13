02af0a8dcc334479d01d4d1693e3eaa325b24777
ZTjack <jack.jiang.scivan@gmail.com>
2020-03-13 17:17:37 +0800

    add prretierr support in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,31 @@
  * @Author: Jack
  * @Date: 2020-03-02 15:41:49
  * @LastEditors: Jack
- * @LastEditTime: 2020-03-13 10:52:08
+ * @LastEditTime: 2020-03-13 16:15:29
  * @Description:
  */
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    sourceType: "module"
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   env: {
     browser: true,
     node: true,
     es6: true
   },
-  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/typescript"],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    '@vue/typescript',
+    'plugin:prettier/recommended'
+  ],
   rules: {
-    "vue/no-v-html": 0,
+    'vue/no-v-html': 0,
     // 每行多少个属性，html
-    "vue/max-attributes-per-line": [
+    'vue/max-attributes-per-line': [
       0,
       {
         singleline: 2,
@@ -31,10 +37,10 @@ module.exports = {
       }
     ],
     // 有的时候ts会 (this as HTMLElement).focus()
-    "no-extra-semi": 0,
-    "vue/html-indent": [
+    'no-extra-semi': 0,
+    'vue/html-indent': [
       0,
-      "tab",
+      'tab',
       {
         attribute: 1,
         baseIndent: 1,
@@ -43,14 +49,14 @@ module.exports = {
         ignores: []
       }
     ],
-    "vue/html-closing-bracket-newline": 0,
-    "vue/html-self-closing": 0,
-    "vue/singleline-html-element-content-newline": [
-      "error",
+    'vue/html-closing-bracket-newline': 0,
+    'vue/html-self-closing': 0,
+    'vue/singleline-html-element-content-newline': [
+      'error',
       {
         ignoreWhenNoAttributes: true,
         ignoreWhenEmpty: true
       }
     ]
   }
-};
+}
